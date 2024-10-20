c742f606d9911ce273ae3885d4c0468b381371d3
litim smail <litimsmail@gmail.com>
2024-10-20 10:19:24 +0100

    Multiple Condition

--- src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
+++ src/main/java/school/mjc/stage0/conditions/task3/IsEnglishSymbolDeterminer.java
@@ -2,5 +2,9 @@ package school.mjc.stage0.conditions.task3;
 
 public class IsEnglishSymbolDeterminer {
     public void isEnglishSymbol(char symbol) {
-    }
+        if (Character.isAlphabetic(symbol)) {
+            System.out.println("English");
+        } else {
+            System.out.println("Non English");
+    }}
 }
